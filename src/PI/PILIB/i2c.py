5fb06bdf7408e11eebdccdc687c85ce2732dd331
Riccardo Vanin <59480824+Greifent@users.noreply.github.com>
2022-10-12 22:16:30 +0200

    Update i2c.py
    If Arduino is turned off while program is running, when Arduino is turned on again it resumes working

--- src/PI/PILIB/i2c.py
+++ src/PI/PILIB/i2c.py
@@ -5,5 +5,8 @@ class i2c:
     def write(temp):
         addr = 0x8 # bus address
         bus = SMBus(1) # indicates /dev/ic2-1
-        bus.write_byte(addr, int(temp)) # send value
+        try:
+            bus.write_byte(addr, int(temp)) # send value
+        except:
+            return -1
 
